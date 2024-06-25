e0cac9e1ac27ef6c97a52d1929b39a2e772c6f9f
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-25 20:04:00 +0300

    updayte

--- net_prog_8/MainWindow.xaml.cs
+++ net_prog_8/MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using System.Text;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace net_prog_8
+namespace net_prog_7.Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const string ServerIP = "10.0.0.139";
+        private const int ServerPort = 8888;
+        private UdpClient udpClient;
+
         public MainWindow()
         {
             InitializeComponent();
+            udpClient = new UdpClient();
+        }
+
+        private async void btnSendRequest_Click(object sender, RoutedEventArgs e)
+        {
+            string request = txtRequest.Text;
+            byte[] requestData = Encoding.ASCII.GetBytes(request);
+
+            try
+            {
+                await udpClient.SendAsync(requestData, requestData.Length, ServerIP, ServerPort);
+                txtLog.AppendText($"Sent request to server: {request}\n");
+
+                
+                UdpReceiveResult result = await udpClient.ReceiveAsync();
+                string response = Encoding.ASCII.GetString(result.Buffer);
+                txtLog.AppendText($"Received response from server: {response}\n");
+            }
+            catch (Exception ex)
+            {
+                txtLog.AppendText($"Error: {ex.Message}\n");
+            }
         }
     }
 }
